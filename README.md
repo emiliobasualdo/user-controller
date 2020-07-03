c6acef5bb7d34a2b103adf584c077de04b7a4059
Emilio Basualdo <Pilobas97@gmail.com>
2020-07-03 19:08:45 +0200

    second verison

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-1) levantar un beanstalk: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/go-getstarted.html
+
+## levantar un beanstalk: 
+https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/go-getstarted.html
+1) meter código en application.go
+2) eb init
+3) eb deploy
+
+- eb open // open the current env endpoint url
+
+## asociar el beanstalk a un api-gateway
+1) levantanar un Load Balancer para el app : https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-nlb-for-vpclink-using-console.html
+
+## deploy
+eb solo deploya código que haya sido commiteado
+1) git commit ...
+2) eb deploy
--- application.go
+++ application.go
@@ -1,19 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "Hello World! Append a name to the URL to say hello. For example, use %s/Mary to say hello to Mary.", r.Host)
-	} else {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}
+type UserProfile struct {
+	Name    	string	`json:"name"`
+	LastName    string	`json:"lastName"`
+	PhoneNumber string	`json:"phoneNumber"`
+}
+
+type Card struct {
+	Holder    string	`json:"holder"`
+	Number    string	`json:"number"`
+	ValidThru string	`json:"validThru"`
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":5000", nil)
+	// Creates a gin router with default middleware:
+	// logger and recovery (crash-free) middleware
+	router := gin.Default()
+	root := router.Group("/")
+	{
+		root.GET("/", statusHandler)
+	}
+	auth := router.Group("/auth")
+	{
+		auth.POST("/login", loginHandler)
+	}
+	instruments := router.Group("/ls")
+	{
+		instruments.GET("/", instrumentsHandler)
+	}
+	log.Fatal(router.Run(":5000"))
 }
+
+func statusHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"version": "v1",
+	})
+}
+
+func loginHandler(c *gin.Context) {
+	profile := UserProfile{"Adolfo", "Olivera", "5491133071114"}
+	c.JSON(http.StatusOK, profile)
+	return
+}
+
+func instrumentsHandler(c *gin.Context) {
+	card1 := Card{"Adolfo Olivera","5555666677778888","07/25"}
+	card2 := Card{"Adolfo Olivera","1111222233334444","06/22"}
+	cards := []Card{card1, card2}
+	c.JSON(http.StatusOK, cards)
+}
+
+
