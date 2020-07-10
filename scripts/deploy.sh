3f622a514a87bc1c166136f37fa724495abbe4ad
Emilio Basualdo <Pilobas97@gmail.com>
2020-07-10 20:26:59 +0200

    deploy bug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 set -e
 echo "\033[34mUPLOADING CONFIG FILES\033[0m"
-cat config-default.yaml | eb ssh --command 'cat > $HOME/config-default.yaml'
+cat config-default.yaml | eb ssh --command 'sudo tee /home/webapp/config-default.yaml >/dev/null'
 echo "\033[34mDEPLOYING\033[0m"
 eb deploy
